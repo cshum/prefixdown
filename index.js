27104f11cdc3e8fb53f9b0a33ca03d9a8f91c66c
Adrian Shum <me@cshum.com>
2015-08-24 16:35:13 +0800

    Switch to standard

--- index.js
+++ index.js
@@ -1,162 +1,159 @@
-var abs          = require('abstract-leveldown'),
-    inherits     = require('util').inherits,
-    xtend        = require('xtend'),
-    iterate      = require('stream-iterate');
+var abs = require('abstract-leveldown')
+var inherits = require('util').inherits
+var xtend = require('xtend')
+var iterate = require('stream-iterate')
 
-var END = '\uffff';
+var END = '\uffff'
 
 function concat (prefix, key) {
-  if (typeof key === 'string') 
-    return prefix + key;
-  if (Buffer.isBuffer(key)) 
-    return Buffer.concat([Buffer(prefix), key]);
-  return prefix + String(key);
+  if (typeof key === 'string') {
+    return prefix + key
+  }
+  if (Buffer.isBuffer(key)) {
+    return Buffer.concat([Buffer(prefix), key])
+  }
+  return prefix + String(key)
 }
 
 function encoding (o) {
   return xtend(o, {
     keyEncoding: o.keyAsBuffer ? 'binary' : 'utf8',
     valueEncoding: (o.asBuffer || o.valueAsBuffer) ? 'binary' : 'utf8'
-  });
+  })
 }
 
 function ltgt (prefix, x) {
-  var r = !!x.reverse;
-
+  var r = !!x.reverse
   var at = {};
-  ['lte','gte','lt','gt','start','end'].forEach(function(key){
-    at[key] = x[key];
-    delete x[key];
-  });
 
-  if(at.gte) x.gte = concat(prefix, at.gte);
-  else if(at.gt) x.gt = concat(prefix, at.gt);
-  else if(at.start && !r) x.gte = concat(prefix, at.start);
-  else if(at.end && r) x.gte = concat(prefix, at.end);
-  else x.gte = at.keyAsBuffer ? Buffer(prefix) : prefix;
+  ['lte', 'gte', 'lt', 'gt', 'start', 'end'].forEach(function (key) {
+    at[key] = x[key]
+    delete x[key]
+  })
 
-  if(at.lte) x.lte = concat(prefix, at.lte);
-  else if(at.lt) x.lt = concat(prefix, at.lt);
-  else if(at.end && !r) x.lte = concat(prefix, at.end);
-  else if(at.start && r) x.lte = concat(prefix, at.start);
-  else x.lte = concat(prefix, at.keyAsBuffer ? Buffer(END) : END);
+  if (at.gte) x.gte = concat(prefix, at.gte)
+  else if (at.gt) x.gt = concat(prefix, at.gt)
+  else if (at.start && !r) x.gte = concat(prefix, at.start)
+  else if (at.end && r) x.gte = concat(prefix, at.end)
+  else x.gte = at.keyAsBuffer ? Buffer(prefix) : prefix
 
-  return x;
+  if (at.lte) x.lte = concat(prefix, at.lte)
+  else if (at.lt) x.lt = concat(prefix, at.lt)
+  else if (at.end && !r) x.lte = concat(prefix, at.end)
+  else if (at.start && r) x.lte = concat(prefix, at.start)
+  else x.lte = concat(prefix, at.keyAsBuffer ? Buffer(END) : END)
+
+  return x
 }
 
 module.exports = function (db) {
-  //reuse prefixdown
-  if(db._prefixdown) return db._prefixdown;
+  // db is levelup instance
 
-  //db is levelup instance
+  // reuse prefixdown
+  if (db._prefixdown) return db._prefixdown
 
   function PrefixIterator (prefix, options) {
-    abs.AbstractIterator.call(this);
+    abs.AbstractIterator.call(this)
 
-    var opts = ltgt(prefix, encoding(options));
+    var opts = ltgt(prefix, encoding(options))
 
-    //should emit key value object
-    opts.keys = true;
-    opts.values = true;
+    // should emit key value object
+    opts.keys = true
+    opts.values = true
 
-    this._stream = db.createReadStream(opts);
-    this._read = iterate(this._stream);
-    this._len = prefix.length;
+    this._stream = db.createReadStream(opts)
+    this._read = iterate(this._stream)
+    this._len = prefix.length
   }
 
-  inherits(PrefixIterator, abs.AbstractIterator);
+  inherits(PrefixIterator, abs.AbstractIterator)
 
   PrefixIterator.prototype._next = function (cb) {
-    var self = this;
-    this._read(function(err, data, next){
-      if(err) return cb(err);
-      if(!data) return cb();
-      next();
-      cb(err, data.key.slice(self._len), data.value);
-    });
-  };
-
-  PrefixIterator.prototype._end = function (cb) {
-    if (this._stream.destroy)
-      this._stream.destroy();
-    process.nextTick(cb);
-  };
-
-  function PrefixDOWN (prefix) {
-    if (!(this instanceof PrefixDOWN))
-      return new PrefixDOWN(prefix);
-
-    abs.AbstractLevelDOWN.call(this, prefix);
-
-    this.prefix = prefix;
+    var self = this
+    this._read(function (err, data, next) {
+      if (err) return cb(err)
+      if (!data) return cb()
+      next()
+      cb(err, data.key.slice(self._len), data.value)
+    })
   }
 
-  inherits(PrefixDOWN, abs.AbstractLevelDOWN);
+  PrefixIterator.prototype._end = function (cb) {
+    if (this._stream.destroy) this._stream.destroy()
+    process.nextTick(cb)
+  }
+
+  function PrefixDOWN (prefix) {
+    if (!(this instanceof PrefixDOWN)) return new PrefixDOWN(prefix)
+
+    abs.AbstractLevelDOWN.call(this, prefix)
+
+    this.prefix = prefix
+  }
+
+  inherits(PrefixDOWN, abs.AbstractLevelDOWN)
 
   PrefixDOWN.prototype._getPrefix = function (options) {
-    //handle prefix options
-    if(options && options.prefix){
-      var prefix = options.prefix;
-      if(prefix === db)
-        return ''; //no prefix for root db
-      if(typeof prefix === 'string')
-        return prefix; //string prefix 
-      //levelup of prefixdown prefix
-      //levelup v2
-      if(prefix._db instanceof PrefixDOWN)
-        return prefix._db.location;
-      //levelup v1
-      if(prefix.options && prefix.options.db === PrefixDOWN)
-        return prefix.location;
+    // handle prefix options
+    if (options && options.prefix) {
+      var prefix = options.prefix
+      // no prefix for root db
+      if (prefix === db) return ''
+      // string prefix
+      if (typeof prefix === 'string') return prefix
+      // levelup of prefixdown prefix
+      // levelup v2
+      if (prefix._db instanceof PrefixDOWN) return prefix._db.location
+      // levelup v1
+      if (prefix.options && prefix.options.db === PrefixDOWN) return prefix.location
     }
-    return this.prefix;
-  };
+    return this.prefix
+  }
 
   PrefixDOWN.prototype._put = function (key, value, options, cb) {
-    if(value === null || value === undefined)
-      value = options.asBuffer ? Buffer(0) : '';
-    db.put(concat(this._getPrefix(options), key), value, encoding(options), cb);
-  };
+    if (value === null || value === undefined) {
+      value = options.asBuffer ? Buffer(0) : ''
+    }
+    db.put(concat(this._getPrefix(options), key), value, encoding(options), cb)
+  }
 
   PrefixDOWN.prototype._get = function (key, options, cb) {
-    db.get(concat(this._getPrefix(options), key), encoding(options), cb);
-  };
+    db.get(concat(this._getPrefix(options), key), encoding(options), cb)
+  }
 
   PrefixDOWN.prototype._del = function (key, options, cb) {
-    db.del(concat(this._getPrefix(options), key), encoding(options), cb);
-  };
+    db.del(concat(this._getPrefix(options), key), encoding(options), cb)
+  }
 
-  PrefixDOWN.prototype._batch = function(operations, options, cb) {
-    if (arguments.length === 0) 
-      return new abs.AbstractChainedBatch(this);
-    if (!Array.isArray(operations)) 
-      return db.batch.apply(null, arguments);
+  PrefixDOWN.prototype._batch = function (operations, options, cb) {
+    if (arguments.length === 0) return new abs.AbstractChainedBatch(this)
+    if (!Array.isArray(operations)) return db.batch.apply(null, arguments)
 
-    var ops = new Array(operations.length);
+    var ops = new Array(operations.length)
     for (var i = 0, l = operations.length; i < l; i++) {
-      var o = operations[i];
-      var isValBuf = Buffer.isBuffer(o.value);
-      var isKeyBuf = Buffer.isBuffer(o.key);
+      var o = operations[i]
+      var isValBuf = Buffer.isBuffer(o.value)
+      var isKeyBuf = Buffer.isBuffer(o.key)
       ops[i] = xtend(o, {
-        type: o.type, 
-        key: concat(this._getPrefix(o), o.key), 
+        type: o.type,
+        key: concat(this._getPrefix(o), o.key),
         value: isValBuf ? o.value : String(o.value),
-        keyEncoding: isKeyBuf ? 'binary':'utf8',
-        valueEncoding: isValBuf ? 'binary':'utf8'
-      });
+        keyEncoding: isKeyBuf ? 'binary' : 'utf8',
+        valueEncoding: isValBuf ? 'binary' : 'utf8'
+      })
     }
-    db.batch(ops, options, cb);
-  };
+    db.batch(ops, options, cb)
+  }
 
   PrefixDOWN.prototype._iterator = function (options) {
-    return new PrefixIterator(this._getPrefix(options), options);
-  };
+    return new PrefixIterator(this._getPrefix(options), options)
+  }
 
   PrefixDOWN.prototype._isBuffer = function (obj) {
-    return Buffer.isBuffer(obj);
-  };
+    return Buffer.isBuffer(obj)
+  }
 
-  db._prefixdown = PrefixDOWN;
+  db._prefixdown = PrefixDOWN
 
-  return PrefixDOWN;
-};
+  return PrefixDOWN
+}
--- test/test.js
+++ test/test.js
@@ -1,54 +1,56 @@
-var test = require('tape');
-var leveldown = require('leveldown');
-var levelup = require('levelup');
-var prefix = require('../');
-var testCommon = require('abstract-leveldown/testCommon');
-var testBuffer = require('memdown/testdata_b64');
+var test = require('tape')
+var leveldown = require('leveldown')
+var levelup = require('levelup')
+var prefix = require('../')
+var testCommon = require('abstract-leveldown/testCommon')
+var testBuffer = require('memdown/testdata_b64')
 
-var db = levelup('test/db', {db: leveldown});
-var prefixdown = prefix(db);
+var db = levelup('test/db', { db: leveldown })
+var prefixdown = prefix(db)
 
-test('batch prefix', function(t){
-  t.plan(4);
-  //prefix as location
-  var dbA = levelup('!a!', {db: prefixdown });
-  var dbB = levelup('!b!', {db: prefixdown });
-  var dbC = levelup('!c!', {db: prefixdown });
+test('batch prefix', function (t) {
+  t.plan(8)
+  // prefix as location
+  var dbA = levelup('!a!', { db: prefixdown })
+  var dbB = levelup('!b!', { db: prefixdown })
+  var dbC = levelup('!c!', { db: prefixdown })
 
   dbA.batch([
     { type: 'put', key: 'foo', value: 'b', prefix: dbB },
     { type: 'put', key: 'foo', value: 'c', prefix: '!c!' },
     { type: 'put', key: 'foo', value: 'a' },
     { type: 'put', key: 'foo', value: 'root', prefix: db }
-  ], function(){
-    db.get('foo', function(err, val){
-      t.equal(val, 'root', 'root levelup prefix');
-    });
-    dbA.get('foo', function(err, val){
-      t.equal(val, 'a', 'default prefix');
-    });
-    dbB.get('foo', function(err, val){
-      t.equal(val, 'b', 'levelup prefixdown prefix');
-    });
-    dbC.get('foo', function(err, val){
-      t.equal(val, 'c', 'string prefix');
-    });
-  });
+  ], function () {
+    db.get('foo', function (err, val) {
+      t.notOk(err)
+      t.equal(val, 'root', 'root levelup prefix')
+    })
+    dbA.get('foo', function (err, val) {
+      t.notOk(err)
+      t.equal(val, 'a', 'default prefix')
+    })
+    dbB.get('foo', function (err, val) {
+      t.notOk(err)
+      t.equal(val, 'b', 'levelup prefixdown prefix')
+    })
+    dbC.get('foo', function (err, val) {
+      t.notOk(err)
+      t.equal(val, 'c', 'string prefix')
+    })
+  })
 
-});
+})
 
-// return;
-//compatibility with basic LevelDOWN API
-
-require('abstract-leveldown/abstract/leveldown-test').args(prefixdown, test, testCommon);
-require('abstract-leveldown/abstract/open-test').args(prefixdown, test, testCommon);
-require('abstract-leveldown/abstract/open-test').open(prefixdown, test, testCommon);
-require('abstract-leveldown/abstract/del-test').all(prefixdown, test, testCommon);
-require('abstract-leveldown/abstract/get-test').all(prefixdown, test, testCommon);
-require('abstract-leveldown/abstract/put-test').all(prefixdown, test, testCommon);
-require('abstract-leveldown/abstract/put-get-del-test').all(prefixdown, test, testCommon, testBuffer);
-require('abstract-leveldown/abstract/batch-test').all(prefixdown, test, testCommon);
-require('abstract-leveldown/abstract/chained-batch-test').all(prefixdown, test, testCommon);
-require('abstract-leveldown/abstract/close-test').close(prefixdown, test, testCommon);
-require('abstract-leveldown/abstract/iterator-test').all(prefixdown, test, testCommon);
-require('abstract-leveldown/abstract/ranges-test').all(prefixdown, test, testCommon);
+// compatibility with basic LevelDOWN API
+require('abstract-leveldown/abstract/leveldown-test').args(prefixdown, test, testCommon)
+require('abstract-leveldown/abstract/open-test').args(prefixdown, test, testCommon)
+require('abstract-leveldown/abstract/open-test').open(prefixdown, test, testCommon)
+require('abstract-leveldown/abstract/del-test').all(prefixdown, test, testCommon)
+require('abstract-leveldown/abstract/get-test').all(prefixdown, test, testCommon)
+require('abstract-leveldown/abstract/put-test').all(prefixdown, test, testCommon)
+require('abstract-leveldown/abstract/put-get-del-test').all(prefixdown, test, testCommon, testBuffer)
+require('abstract-leveldown/abstract/batch-test').all(prefixdown, test, testCommon)
+require('abstract-leveldown/abstract/chained-batch-test').all(prefixdown, test, testCommon)
+require('abstract-leveldown/abstract/close-test').close(prefixdown, test, testCommon)
+require('abstract-leveldown/abstract/iterator-test').all(prefixdown, test, testCommon)
+require('abstract-leveldown/abstract/ranges-test').all(prefixdown, test, testCommon)
